59fb9f5c03067af7dbd7ede9014d9d1de8be2448
David-python80 <challmer80@gmail.com>
2024-09-05 22:04:06 +0200

    loginpage och dashboardpage

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from './pages/login-page';
+import { DashboardPage } from './pages/dashboard-page';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.describe('Test suite 01', () => {
+  test('perform login and logout', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const dashboardPage = new DashboardPage(page);
+    await loginPage.goto();
+    await loginPage.performLogin(`${process.env.TEST_USERNAME}`, `${process.env.TEST_PASSWORD}`);
+    await expect(page.getByRole('heading', { name: 'Tester Hotel Overview' })).toBeVisible();
+    
+    //await dashboardPage.goToRoomView();
+    //await dashboardPage.goToClientView();
+    //await dashboardPage.goToBillsView();
+    await dashboardPage.goToReservationView();
+    
+    await page.waitForTimeout(1000);
 });
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
---
+++ tests/pages/dashboard-page.ts
@@ -0,0 +1,48 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+
+export class DashboardPage {
+    // attributes
+    readonly page: Page;
+    readonly logoutButton: Locator;
+    readonly roomsViewButton: Locator;
+    readonly clientsViewButton: Locator;
+    readonly billsViewButton: Locator;
+    readonly reservationsViewButton: Locator;
+
+    // const
+    constructor(page: Page) {
+        this.page = page;
+        this.logoutButton = page.getByRole('button', { name: 'Logout' });
+        this.roomsViewButton = page.locator('#app > div > div > div:nth-child(1) > a');
+        this.clientsViewButton = page.locator('#app > div > div > div:nth-child(2) > a');
+        this.billsViewButton = page.locator('#app > div > div > div:nth-child(3) > a');
+        this.reservationsViewButton = page.locator('#app > div > div > div:nth-child(4) > a');
+}
+
+    // methods / functions
+    async goToRoomView(){
+    
+        await this.roomsViewButton.click();
+    }
+    async goToClientView(){
+
+        await this.clientsViewButton.click();
+    }
+    async goToBillsView(){
+
+        await this.billsViewButton.click();
+    }
+    async goToReservationView(){
+
+        await this.reservationsViewButton.click();
+    }
+    async performLogout() {
+    // click logout button
+    await this.logoutButton.click();
+}
+
+    // async pageObjectModel() {
+    //     await this.getStarted();
+    //     await this.pomLink.click();
+    // }
+}
