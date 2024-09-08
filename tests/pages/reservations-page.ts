fcb7db11ee99b32c360aca3edf9f02ff6b1a886f
Mikey Mansour <sebastianmansour96rgmail.com>
2024-09-09 00:41:18 +0200

    first draft of most tests done and some updates to reservations page

--- tests/pages/reservations-page.ts
+++ tests/pages/reservations-page.ts
@@ -4,12 +4,16 @@ export class ReservationsPage {
     readonly page: Page;
     readonly pageHeading: Locator;
     readonly createReservationButton: Locator;
+    readonly reservationOptionsButton: Locator;
+    readonly reservationDeleteButton: Locator;
     readonly backButton: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.pageHeading = page.getByText('Reservations');
         this.createReservationButton = page.getByRole('link', { name: 'Create Reservation' });
+        this.reservationOptionsButton = page.getByRole('img').first();
+        this.reservationDeleteButton = page.getByText('Delete');
         this.backButton = page.getByRole('link', { name: 'Back' });
     }
 
@@ -21,4 +25,8 @@ export class ReservationsPage {
         await this.backButton.click();
     }
 
+    async deleteReservation(){
+        await this.reservationOptionsButton.click();
+        await this.reservationDeleteButton.click();
+    }
 }
