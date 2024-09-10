fc70cc489bd03bd96f15b69aaabda3d0eb1d6749
Malin Hansen <hansen.malin@icloud.com>
2024-09-10 21:42:24 +0200

    updated script with app name

--- run-tests.sh
+++ run-tests.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-echo Starting the application...
-sleep 1s
-container_id=$(docker run -d -p 3000:3000 my-vue-app)
-docker exec $container_id ls -la
+echo Starting application...
 sleep 2s
+docker run --name hotelapp -d -p 3000:3000 my-vue-app
+docker exec hotelapp ls -la
+sleep 4s
 echo Initiating test session...
-sleep 1s
-echo Generating test data...
 sleep 2s
-echo Running tests...
-sleep 1s
 npm run testing random.spec.ts
-sleep 7s
-echo Terminating application...
-docker stop $container_id
 sleep 3s
 echo Testing complete!
+sleep 2s
+echo Terminating application...
+docker stop hotelapp
+docker rm hotelapp
+sleep 3s
+echo Opening test report...
+sleep 2s
+npm run test-report
