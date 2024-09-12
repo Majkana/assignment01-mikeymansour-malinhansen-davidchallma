212a87741974eed3c547326e6eefde2e7ae46bd3
Malin Hansen <hansen.malin@icloud.com>
2024-09-12 17:18:35 +0200

    refined script efter changes elsewhere

--- run-tests.sh
+++ run-tests.sh
@@ -2,18 +2,17 @@
 echo Starting application...
 sleep 2s
 docker run --name hotelapp -d -p 3000:3000 my-vue-app
-docker exec hotelapp ls -la
 sleep 4s
 echo Initiating test session...
 sleep 2s
-npm run testing random.spec.ts
+npm run testing
 sleep 3s
 echo Testing complete!
-sleep 2s
+sleep 3s
 echo Terminating application...
 docker stop hotelapp
 docker rm hotelapp
-sleep 3s
+sleep 4s
 echo Opening test report...
 sleep 2s
 npm run test-report
