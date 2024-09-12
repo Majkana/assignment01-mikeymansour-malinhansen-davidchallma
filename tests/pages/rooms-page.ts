c80b4220e4078ebeed7e47797f3a9dd3dce72e7e
Malin Hansen <hansen.malin@icloud.com>
2024-09-12 15:31:33 +0200

    removed elements not in use

--- tests/pages/rooms-page.ts
+++ tests/pages/rooms-page.ts
@@ -1,5 +1,4 @@
 import { type Locator, type Page } from '@playwright/test';
-import { faker } from '@faker-js/faker';
 
 export class RoomsPage {
     readonly page: Page;
@@ -7,21 +6,14 @@ export class RoomsPage {
     readonly pageHeading: Locator;
     readonly createRoomButton: Locator;
     readonly roomElements: Locator;
-    readonly roomOptionsButton: Locator;
-    readonly editRoomButton: Locator;
-    readonly deleteRoomButton: Locator;
     readonly backButton: Locator;
     
     constructor(page: Page) {
-        const roomNumber = faker.number.int({ min: 1, max: 20 });
         this.page = page;
         this.pageUrl = (`${process.env.BASE_URL}/rooms`);
         this.pageHeading = page.getByText('Rooms', {exact: true});
         this.createRoomButton = page.getByRole('link', { name: 'Create Room' });
         this.roomElements = page.locator('#app > div > div.rooms > div.card.room');
-        this.roomOptionsButton = page.locator('div:nth-child(2) > .action > img');
-        this.editRoomButton = page.locator(`div:nth-child(${roomNumber}) > div.menu > a:nth-child(1)`);
-        this.deleteRoomButton = page.locator('div:nth-child(1) > div.menu > a:nth-child(2)');
         this.backButton = page.getByRole('link', { name: 'Back' });
     };
 
